553c56f7ce04919c3ea2a5c8855a3520acd49769
Diego O. da Silva <doliveira@geniussistemas.com.br>
2021-07-19 22:28:29 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace appContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

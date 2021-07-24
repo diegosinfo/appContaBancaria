70702497d69f55d653f0f2c8434198595de097d4
Diego O. da Silva <doliveira@geniussistemas.com.br>
2021-07-24 11:28:30 -0300

    implementado o metodo de Sacar na classe contaBancaria.

--- Entidades/ContaBancaria.cs
+++ Entidades/ContaBancaria.cs
@@ -1,28 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using appContaBancaria.Enum;
+﻿using appContaBancaria.Enum;
+using System;
+
 
 namespace appContaBancaria.Entidades
 {
     class ContaBancaria
     {
-       private TipoConta TipoConta { get; set; }
-        
+        private TipoConta TipoConta { get; set; }
         private string Nome { get; set; }
-
         private double Saldo { get; set; }
+        private double Limite { get; set; }
 
-        private double Credito { get; set; }
+        private double LimiteUtilizado { get; set; }
+     
 
-        public ContaBancaria(TipoConta tipoConta, string nome, double saldo, double credito)
+        public ContaBancaria(TipoConta tipoConta, string nome, double saldo, double limite)
         {
             TipoConta = tipoConta;
             Nome = nome;
             Saldo = saldo;
-            Credito = credito;
+            Limite = limite;
+            LimiteUtilizado = 0;
         }
+
+        public bool Sacar(double valorSaque)
+        {
+            bool operacaoRealizada = false;
+
+            //Se há saldo suficiente, não precisa utilizar o limite.
+            if(valorSaque <= Saldo)
+            {
+                Saldo -= valorSaque;
+                operacaoRealizada = true;
+        
+            }
+            else if(valorSaque <= (Saldo + (Limite - LimiteUtilizado)))
+            {
+                if (Saldo > 0.00 && Saldo <= valorSaque)
+                {
+                    valorSaque -= Saldo;
+                    Saldo = 0.00;
+                }
+
+                if(valorSaque <= (Limite - LimiteUtilizado))
+                {
+                    LimiteUtilizado += valorSaque;
+                    operacaoRealizada = true;
+                }
+                else
+                {
+                    operacaoRealizada = false;
+                }
+            }
+
+
+            if (operacaoRealizada)
+            {
+                Console.WriteLine("Saldo atual da conta de {0} é {1} - Limite Disponivel: {2}", Nome, 
+                                LimiteUtilizado == 0.00? Saldo.ToString("C") : ((-1) * LimiteUtilizado).ToString("C"), (Limite - LimiteUtilizado).ToString("C"));
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+
+            return operacaoRealizada;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using appContaBancaria.Entidades;
+using appContaBancaria.Enum;
 
 namespace appContaBancaria
 {
@@ -7,6 +9,12 @@ namespace appContaBancaria
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            ContaBancaria minhaConta = new ContaBancaria(TipoConta.PessoaFisica, "Diego", 600, 100);
+
+
+            minhaConta.Sacar(600.00);
+            minhaConta.Sacar(1.5);
         }
     }
 }
